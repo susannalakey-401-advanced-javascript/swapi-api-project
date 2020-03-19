09f12fa7122996a1112053890dfe71e580109b6c
Susanna Lakey <susannalakey@gmail.com>
2020-03-18 20:23:10 -0700

    Updates

---
+++ precache-manifest.4a4b2da5a2916d1c207eba4b8adaec34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c38130b2edd86c34557104bd192a32d",
+    "url": "/swapi-api-project/index.html"
+  },
+  {
+    "revision": "00b1ef9981512e1a1820",
+    "url": "/swapi-api-project/static/css/main.ec1b1b7e.chunk.css"
+  },
+  {
+    "revision": "82673ccf01004cf63a30",
+    "url": "/swapi-api-project/static/js/2.f2c74ba7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/swapi-api-project/static/js/2.f2c74ba7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00b1ef9981512e1a1820",
+    "url": "/swapi-api-project/static/js/main.d9a915f7.chunk.js"
+  },
+  {
+    "revision": "984c2456b7ef91b1def8",
+    "url": "/swapi-api-project/static/js/runtime-main.2cfa7dbd.js"
+  }
+]);
